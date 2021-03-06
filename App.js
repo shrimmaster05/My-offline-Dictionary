cf9a2156a7fb6ec8828bdd90af40cbd3b203f45f
shrimmaster05 <76813412+shrimmaster05@users.noreply.github.com>
2021-03-06 13:34:31 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import db from './localdb';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      chunks: [],
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor={'cyan'}
+          centerComponent={{
+            text: 'POCKET DICTIONARAY',
+            style: { color: 'purple', fontSize: 20 },
+          }}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({ text: text });
+          }}
+          value={this.state.text}
+        />
+        <TouchableOpacity
+          style={styles.goButton}
+          onPress={() => {
+            this.setState({ chunks: db[this.state.text].chunks });
+          }}>
+          <Text style={styles.buttonText}>GO</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map(item => {
+            return (
+              <TouchableOpacity
+              style={styles.chunkButton}
+              >
+              <Text style={styles.displayText}>{item}</Text>
+              </TouchableOpacity>
+              );
+          })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF69B4',
+  },
+  inputBox: {
+    marginTop: 50,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white'
+  },
+  imageIcon: {
+    width: 150,
+    height: 150,
+    marginLeft: 95,
+  },
+  chunkButton:{
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+    margin: 5,
+    backgroundColor: 'purple'
+  }
+});
---
+++ localdb.js
@@ -0,0 +1,13 @@
+const db = {
+  the: { chunks: ['determiner', 'denoting one or more people'], phones: ['DH', 'AH'] },
+  india: { chunks: ['noun', 'A country name in Asia'], phones: ['AH', 'V'] },
+  hello: { chunks: ['intergection', 'used as a greeting', 'or to begin a conversaition'], phones: ['AH', 'N', 'D'] },
+  to: { chunks: ['t', 'o'], phones: ['T', 'UW'] },
+  a: { chunks: ['a'], phones: ['AH'] },
+  in: { chunks: ['i', 'n'], phones: ['IH', 'N'] },
+  for: { chunks: ['f', 'o', 'r'], phones: ['F', 'AO', 'R'] },
+  is: { chunks: ['i', 's'], phones: ['IH', 'Z'] },
+  on: { chunks: ['o', 'n'], phones: ['AA', 'N'] },
+  
+};
+export default db;
